d0d0fc509555573396c3b8ea0a7a877547616bbb
Khalid Mihlar <khalid.mihlar@gmail.com>
2026-02-17 14:54:17 -0700

    Update setup-reconcilia-mac.sh

--- setup-reconcilia-mac.sh
+++ setup-reconcilia-mac.sh
@@ -15,7 +15,7 @@ BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
 # Configuration
-REPO_URL="git@github.com:khalidmihlar/reconcilia-app.git"
+REPO_URL="https://github.com/khalidmihlar/reconcilia-app.git"
 APP_DIR="reconcilia-app"
 REQUIRED_NODE_VERSION="18"
 
